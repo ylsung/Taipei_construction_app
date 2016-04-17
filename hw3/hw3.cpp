7ff321b007cc52cb738c3cf898cf5f6a0f6a4ace
b01504044 <b01504044@linux6.csie.ntu.edu.tw>
2016-04-17 23:26:01 +0800

    hw3_v2

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -129,7 +129,7 @@ int main(){
 	stack<int**> DFSstack;
 	//DFSstack.push(nonogram);
 	//int** NowNonogram;
-	for(int i = 0; i < row; i++){
+	/*for(int i = 0; i < row; i++){
 		for(int j = 0; j < column; j++){
 			cout << nonogram[i][j] << " ";
 		}
